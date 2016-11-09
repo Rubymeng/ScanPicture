6a0528254406e2a76f057a5cf3b9e930f8a7380a
tyhmeng <tyhmeng@163,com>
2016-11-09 11:00:19 +0800

    Initial Commit

---
+++ ScanPicture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScanPicture
+//
+//  Created by tyhmeng on 16/11/9.
+//  Copyright © 2016年 tyhmeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
